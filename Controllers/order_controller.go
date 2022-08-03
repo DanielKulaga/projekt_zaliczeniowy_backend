cd1bf79ea2b228796830574624904b5fe16d61ea
DanielKulaga <danielkulaga@outlook.com>
2022-08-04 00:43:20 +0200

    add order from database

--- Controllers/order_controller.go
+++ Controllers/order_controller.go
@@ -11,7 +11,7 @@ import (
 func CreateOrderToDatabase(c echo.Context) error {
 	orderDetails := new(Models.Order)
 
-	fmt.Printf("Add new order details \n")
+	fmt.Printf("Add new order to databse \n")
 
 	if err := c.Bind(orderDetails); err != nil {
 		return c.String(http.StatusBadRequest, "Bad order details "+err.Error())
@@ -20,4 +20,17 @@ func CreateOrderToDatabase(c echo.Context) error {
 	Database.Database.Create(orderDetails)
 
 	return c.JSON(http.StatusOK, orderDetails)
+}
+
+func GetOrderFromDatabase(c echo.Context) error {
+	id := c.Param("id")
+	var order Models.Order
+
+	fmt.Printf("Get order id: " + id + "\n")
+
+	if result := Database.Database.First(&order, id); result.Error != nil {
+		return c.String(http.StatusNotFound, "Database Error")
+	}
+
+	return c.JSON(http.StatusOK, order)
 }
